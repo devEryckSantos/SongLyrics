e4361b6ecb4363a810c3ca163209e27e963868b9
Eryck Santos <dev.erycksantos@gmail.com>
2025-01-30 11:00:53 -0300

    Added streamlit library. Structuring web page.

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import requests
+import streamlit as st
 
 
-endpoint = "https://api.lyrics.ovh/v1/sia/chandelier"
+st.image("https://imgur.com/SmktDIH.png")
+st.title("Song Lyrics")
+  
+
+singer = st.text_input("Digite o nome do cantor/banda: ", key="singer")
+song = st.text_input("Digite o nome da música: ", key="song")
+search = st.button("Pesquisar", key="search")
+
+endpoint = f"https://api.lyrics.ovh/v1/{singer}/{song}"
 
 response = requests.get(endpoint)
 
-print(response.json()["lyrics"])
